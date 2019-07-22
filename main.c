57fe01218a00eadf67794c18f86464cf68334549
Mandar Kulkarni <52269075+mandar6988@users.noreply.github.com>
2019-07-22 21:58:07 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <LPC21xx.H> 
+
+void pll_init()
+{
+ PLLCON = 0x01;  
+ PLLCFG = 0x24;  
+ PLLFEED = 0xAA;  
+ PLLFEED = 0x55;
+ 
+ while(!(PLLSTAT & 1<<10));
+ 
+ PLLCON = 0x03;   
+ PLLFEED = 0xAA;  
+ PLLFEED = 0x55;
+ VPBDIV = 0x01; 
+}
+void timer0_init(void)
+{
+   	T0TCR = 0x00;  
+    T0PR = 59999;  
+            
+    T0TCR = 0x02;   
+}
+void delay(unsigned int counts) //Using Timer0
+{
+    T0TCR = 0x02;       
+    T0TCR = 0x01;       
+   
+    while(T0TC < counts);
+   
+    T0TCR = 0x00;       
+	T0TC=0x00;
+}
+
+int main()
+{
+pll_init();
+timer0_init()
+	
+	IODIR0=0xFFFFFFFF;
+	while(1)
+	{
+	 IOSET0=0xFFFFFFFF;
+	 delay(1000);
+	 IOCLR0=0xFFFFFFFF;
+	 delay(1000);
+	 }
+}
