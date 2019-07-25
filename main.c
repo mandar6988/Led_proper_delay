f1be433d5f513693610d171bd91fff07e06feebb
Mandar Kulkarni <52269075+mandar6988@users.noreply.github.com>
2019-07-25 22:58:43 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <LPC21xx.H> 
 
-void pll_init()
+void pll_init()// initialise PLL function
 {
  PLLCON = 0x01;  
  PLLCFG = 0x24;  
@@ -14,14 +14,14 @@ void pll_init()
  PLLFEED = 0x55;
  VPBDIV = 0x01; 
 }
-void timer0_init(void)
+void timer0_init(void)// timer initialisation
 {
    	T0TCR = 0x00;  
     T0PR = 59999;  
             
     T0TCR = 0x02;   
 }
-void delay(unsigned int counts) //Using Timer0
+void delay(unsigned int counts) //Creating daleay of Timer0
 {
     T0TCR = 0x02;       
     T0TCR = 0x01;       
@@ -40,9 +40,9 @@ timer0_init()
 	IODIR0=0xFFFFFFFF;
 	while(1)
 	{
-	 IOSET0=0xFFFFFFFF;
+	 IOSET0=0xFFFFFFFF;// Set all pins of port 0
 	 delay(1000);
-	 IOCLR0=0xFFFFFFFF;
+	 IOCLR0=0xFFFFFFFF;// Clear all pins of port 0
 	 delay(1000);
 	 }
 }
